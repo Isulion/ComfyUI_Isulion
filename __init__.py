9ab9d6efc91d237ca0e3135a569542535e79df57
Isulion <138410193+Isulion@users.noreply.github.com>
2024-10-31 19:27:53 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,13 +1,20 @@
 from .isulion_prompt_generator import IsulionPromptGenerator
 from .isulion_OllamaGenerate import OllamaGenerate
+from .isulion_animal_generator import IsulionAnimalRandom
+from .isulion_cute_animal_generator import IsulionCuteAnimalRandom
 
 NODE_CLASS_MAPPINGS = {
     "IsulionPromptGenerator": IsulionPromptGenerator,
-    "OllamaGenerate": OllamaGenerate
+    "OllamaGenerate": OllamaGenerate,
+    "AnimalGenerator": IsulionAnimalRandom,
+    "CuteAnimalGenerator": IsulionCuteAnimalRandom
 }
 
 # Optional: Provide a custom display name for the node
 NODE_DISPLAY_NAME_MAPPINGS = {
     "IsulionPromptGenerator": "Isulion Prompt Generator",
-    "OllamaGenerate": "Isulion Ollama Generate"
+    "OllamaGenerate": "Isulion Ollama Generate",
+    "AnimalGenerator": "Isulion Animal Selector",
+    "AnimalGenerator": "Isulion Cute Animal Selector"
+    
 }
