4a071036c3220167f614785577b28f20bcd993ba
xobiomesh <gr33nturnip@gmail.com>
2024-10-31 03:14:40 +0100

    init

--- __init__.py
+++ __init__.py
@@ -1,13 +1,16 @@
 from .isulion_prompt_generator import IsulionPromptGenerator
 from .isulion_OllamaGenerate import OllamaGenerate
+from .ixulion_ollamaModelSelect import OllamaModelSelector
 
 NODE_CLASS_MAPPINGS = {
     "IsulionPromptGenerator": IsulionPromptGenerator,
-    "OllamaGenerate": OllamaGenerate
+    "OllamaGenerate": OllamaGenerate,
+    "OllamaModelSelector": OllamaModelSelector
 }
 
 # Optional: Provide a custom display name for the node
 NODE_DISPLAY_NAME_MAPPINGS = {
     "IsulionPromptGenerator": "Isulion Prompt Generator",
-    "OllamaGenerate": "Isulion Ollama Generate"
+    "OllamaGenerate": "Isulion Ollama Generate",
+    "OllamaModelSelector": "Ollama Model Selector"
 }
